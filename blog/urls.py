9b623b2730df830f89e372eb85b7476345cb3c5b
dan <danlove99@hotmail.co.uk>
2020-03-18 22:51:11 +0000

    added incomplete homepage

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-path('/oldest', views.oldest, name='oldest'),
-path('/python', views.python, name='python'),
-path('/sqlnosql', views.sqlnosql, name='sqlnosql'),
-path('/web', views.webdevelpment, name='web'),
-path('/scripting', views.scripting, name='scripting'),
+    path('', views.home, name='home'),
+    path('/post_list', views.post_list, name='post_list'),
+    path('/oldest', views.oldest, name='oldest'),
+    path('/python', views.python, name='python'),
+    path('/sqlnosql', views.sqlnosql, name='sqlnosql'),
+    path('/web', views.webdevelpment, name='web'),
+    path('/scripting', views.scripting, name='scripting'),
 ]
