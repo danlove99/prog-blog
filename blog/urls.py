78fe6add87efc1e1ff213d2d0ae58e90bfbc7a96
dan <danlove99@hotmail.co.uk>
2020-03-18 08:53:24 +0000

    Added a filter for the posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+path('/python', views.python, name='python'),
+path('/sqlnosql', views.sqlnosql, name='sqlnosql'),
+path('/web', views.webdevelpment, name='web'),
+path('/scripting', views.scripting, name='scripting'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,19 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def python(request):
+    posts = Post.objects.filter(related_language="python")
+    return render(request, 'blog/python.html', {'posts': posts})
+
+def sqlnosql(request):
+    posts = Post.objects.filter(related_language="sqlnosql")
+    return render(request, 'blog/sqlnosql.html', {'posts': posts})
+
+def webdevelpment(request):
+    posts = Post.objects.filter(related_language="htmlcssjavascript")
+    return render(request, 'blog/web.html', {'posts': posts})
+
+def scripting(request):
+    posts = Post.objects.filter(related_language="scripting")
+    return render(request, 'blog/scripting.html', {'posts': posts})
