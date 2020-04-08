1dc383df7d6359da24f425a0a568b02ad587cae3
dan <danlove99@hotmail.co.uk>
2020-04-08 09:46:47 +0100

    fixed url issues

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('/post_list', views.post_list, name='post_list'),
-    path('/projects', views.projects, name='projects'),
-    path('/oldest', views.oldest, name='oldest'),
-    path('/python', views.python, name='python'),
-    path('/sqlnosql', views.sqlnosql, name='sqlnosql'),
-    path('/web', views.webdevelpment, name='web'),
-    path('/scripting', views.scripting, name='scripting'),
+    path(r'', views.home, name='home'),
+    path(r'^/post_list$', views.post_list, name='post_list'),
+    path(r'^/projects$', views.projects, name='projects'),
+    path(r'^/oldest$', views.oldest, name='oldest'),
+    path(r'^/python$', views.python, name='python'),
+    path(r'^/sqlnosql$', views.sqlnosql, name='sqlnosql'),
+    path(r'^/web$', views.webdevelpment, name='web'),
+    path(r'^/scripting$', views.scripting, name='scripting'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
