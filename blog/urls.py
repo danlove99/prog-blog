b3a95d3873794691570b9748af47baaf65b24e8d
dan <danlove99@hotmail.co.uk>
2020-04-25 09:59:35 +0100

    completed projects page

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path(r'', views.home, name='home'),
     path(r'^/about$', views.about, name='about'),
-    path(r'^/projects$', views.projects, name='projects'),
+    path('/projects$', views.projects, name='projects'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
